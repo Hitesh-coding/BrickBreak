8cf538f64f416cb9a2404f25db789c8b7f08b66f
Hitesh-coding <hiteshkonduru1@gmail.com>
2024-12-07 00:15:13 +0000

    commit -m “submission 12/05/24”

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,13 +1,113 @@
+//
+//  Class author:  Hitesh Konduru
+//  Date created:  12/3/24
+//  General description: This class creates all the methods and constructors to allow the ball to be created and have its movements.
+//
+
+
 import java.awt.*;
 
 public class Ball {
 	//your code here!
 	
 	//don't forget you need instance variables:
+	private int xPos;
+	private int yPos;
+	private int size;
+	private int XVelocity;
+	private int YVelocity;
+
+
 	
-	
+
+
 	//constructor(s):
-	
+	public Ball(int initXPos, int initYPos, int initSize){
+	xPos = initXPos;
+	yPos = initYPos;
+	 size = initSize;
+
+	 XVelocity=1;
+	 YVelocity=1;
+	}
 	//methods:
+//
+//  Pre-condition: not null
+//  Post-condition:a yellow ball will be created 
+//
+public void draw(Graphics g){
+	g.setColor(Color.yellow); // call first to set the desired color
+	g.fillOval(xPos , yPos, size, size); // draws an oval (ball)
+}
+
+public int getSize(){
+	return size;
+}
+
+   public void setSize(int size)
+    {
+        size  = this.size;
+    }
+	public void setX(int xPos3)
+	{
+		xPos = xPos3;
+	}
+	public int getXpos(){
+		return xPos;
+	}
+	
+	  
+		public void setY(int yPos3)
+			{
+				yPos = yPos3;
+			}
+
+	public int getYpos(){
+			return yPos;
+		}
+		
+    
+
+
+	public int getXVelocity(){
+		return XVelocity;
+	}
+
+	
+public void setXVelocity(int XVelocity)
+		{
+			XVelocity= this.XVelocity;
+		}
+//
+//  Pre-condition: none
+//  Post-condition:the ball will move in the horizontally opposite direction
+//
+public void reverseX() {
+	XVelocity = -XVelocity;
+	
+}
+public int getYVelocity() {
+	return YVelocity;
+}
+public void setYVelocity(int YVelocity)
+		{
+			 YVelocity = this.YVelocity;
+		}
+//
+//  Pre-condition: none
+//  Post-condition:the ball will move in the vertically opposite direction
+//
+public void reverseY() {
+	YVelocity = -YVelocity;
+
+}
+//
+//  Pre-condition: none
+//  Post-condition:the ball will move horizontally and vertically
+//
+public void move() {
+	xPos += XVelocity;
+	yPos += YVelocity;
+}
 
 }
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,4 +1,8 @@
-import java.awt.Graphics;
+//
+//  Class author:  Hitesh Konduru
+//  Date created:  12/3/24
+//  General description: This class creates all the methods and constructors to allow the paddle to be created and have its movements.
+//
 import java.awt.*;
 
 public class Paddle {
@@ -6,11 +10,91 @@ public class Paddle {
 	//your code here!
 	
 	//don't forget you need instance variables:
-	
+private int x;
+private int y;
+private int width; 
+private int height;
+private int velocity;
+
+
 	
 	//constructor(s):
-	
+	public Paddle(int initX, int initY, int initWidth, int initHeight){
+		x = initX;
+		y = initY;
+		width = initWidth;
+		height = initHeight;
+		}
 	//methods:
-	
-	
+//
+//  Pre-condition: not null
+//  Post-condition:an orange paddle will be created 
+//
+
+	public void draw(Graphics g){
+	g.setColor(Color.orange); // call first to set the desired color
+	g.fillRect(x, y, width, height); // draws a paddle(player)
+}
+public void setX(int x1){
+	x = x1;
+}
+public int getX(){
+return x;
+}	
+public void setY(int y1){
+	y = y1;
+}
+public int getY(){
+return y;
+}	
+public void setWidth(int width){
+	width = this.width;
+}
+public int getWidth(){
+return width;
+}	
+public void setHeight(int height){
+	height = this.height;
+}
+public int getHeight(){
+return height;
+}	
+
+public void setVelocity(int velocity) {
+ velocity = this.velocity;
+}
+public int getVelocity(){
+	return velocity;
+}
+//
+//  Pre-condition: none
+//  Post-condition:the paddle will move horizontally, and it won't go too slow or too fast and be sent off of the screen based on the boolean limitations
+//
+public void move() {
+	x += velocity;
+	if (x < 0) {x = 0;}
+	if (x > 600) {x = 600;}
+	if(velocity > 0){
+		velocity --;
+	}
+	if(velocity<0){
+		velocity ++;
+	}
+}
+//
+//  Pre-condition: a single int number to add the velocity needs to be entered when this function is called
+//  Post-condition:the velocity of the paddle will get something added to it
+//
+public int addVelocity(int velocity1) {
+	velocity += velocity1;
+	return velocity;
+}
+
+
+
+
+
+
+
+
 }
